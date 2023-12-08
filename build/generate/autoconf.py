b20cd9f1d6303a65bfe354d70f06426bbf1520a1
Conke Hu <conke.hu@maxwit.com>
2023-12-08 10:21:34 -0800

    .py files: use python3 as interpreter
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- build/generate/autoconf.py
+++ build/generate/autoconf.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 #
 # Usage:
 #   $0 .config autoconf.h
--- build/generate/defconfig.py
+++ build/generate/defconfig.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # The Main Program for Building Embedded Development Environment
 #
